274f6f725726403b4988d5ba827cbac2e359b8ff
JamesBremner <james@ravenspoint.com>
2023-06-06 20:25:39 -0400

    Display all combos

--- src/main.cpp
+++ src/main.cpp
@@ -45,18 +45,24 @@ public:
     /// @param S shape doing the drawings
     void draw(wex::shapes &S);
 
+    void incCurCombo();
+    void drawCombo(
+        wex::label &lbCombo,
+        wex::shapes &S);
+
 private:
-    typedef std::vector<double> rect_ltrb_t;        /// left, top, right, bottom of rectangle
-    std::vector<double> myVert;                     /// vertical line spacing
-    std::vector<double> myHorz;                     /// horizontal line spacing
-    std::vector<std::pair<int, int>> myForbidden;   /// indices of forbidden cells
-    std::pair<int, int> myStartCell;                /// indices of start cell
-    std::pair<double, double> myMaxCombSize;        /// maximum valid combination size
-    std::vector<std::vector<std::pair<int, int>>> myListComb;   /// list of valid combinations
+    typedef std::vector<double> rect_ltrb_t;                  /// left, top, right, bottom of rectangle
+    std::vector<double> myVert;                               /// vertical line spacing
+    std::vector<double> myHorz;                               /// horizontal line spacing
+    std::vector<std::pair<int, int>> myForbidden;             /// indices of forbidden cells
+    std::pair<int, int> myStartCell;                          /// indices of start cell
+    std::pair<double, double> myMaxCombSize;                  /// maximum valid combination size
+    std::vector<std::vector<std::pair<int, int>>> myListComb; /// list of valid combinations
+    int myCurComb;
 
     /// @brief left, top, right, bottom of cell
-    /// @param locationIndices 
-    /// @return 
+    /// @param locationIndices
+    /// @return
     rect_ltrb_t rectDim(const std::pair<int, int> &locationIndices);
 
     /// @brief true if cell is forbidden
@@ -74,13 +80,18 @@ private:
         rect_ltrb_t out);
 
     /// @brief draw a rectangle
-    /// @param S 
-    /// @param scale 
-    /// @param r 
+    /// @param S
+    /// @param scale
+    /// @param r
     static void drawRectangle(
         wex::shapes &S,
         double scale,
         const rect_ltrb_t &r);
+
+    static double scale()
+    {
+        return 40;
+    }
 };
 
 class cGUI : public cStarterGUI
@@ -89,7 +100,9 @@ public:
     cGUI()
         : cStarterGUI(
               "Starter",
-              {50, 50, 1000, 500})
+              {50, 50, 1000, 500}),
+          nextComboButton(wex::maker::make<wex::button>(fm)),
+          lbCombo(wex::maker::make<wex::label>(fm))
     {
         myGrid.setVert({0.5, 1, 1.5, 2, 2.5, 3, 2.5, 2});
         myGrid.setHorz({0.5, 1, 1.5, 2, 2.5, 3, 2.5, 2});
@@ -104,7 +117,18 @@ public:
             {
                 wex::shapes S(ps);
                 myGrid.draw(S);
+                myGrid.drawCombo(lbCombo, S);
             });
+        nextComboButton.move(700, 50, 100, 30);
+        nextComboButton.text("Next Combo");
+        nextComboButton.events().click(
+            [&]
+            {
+                myGrid.incCurCombo();
+                fm.update();
+            });
+        lbCombo.move(650, 100, 200, 100);
+        lbCombo.text("");
 
         show();
         run();
@@ -112,6 +136,8 @@ public:
 
 private:
     cGrid myGrid;
+    wex::button &nextComboButton;
+    wex::label &lbCombo;
 };
 
 void cGrid::setHorz(const std::vector<double> &h)
@@ -136,40 +162,54 @@ void cGrid::setVert(const std::vector<double> &v)
     myVert.push_back(l);
 }
 
+void cGrid::incCurCombo()
+{
+    myCurComb++;
+    if (myCurComb >= myListComb.size())
+        myCurComb = 0;
+}
+
 void cGrid::draw(wex::shapes &S)
 {
-    double scale = 40;
+    double sc = scale();
     for (auto h : myHorz)
     {
-        S.line({0, (int)(scale * h), (int)(scale * myVert.back()), (int)(scale * h)});
+        S.line({0, (int)(sc * h), (int)(sc * myVert.back()), (int)(sc * h)});
     }
     for (auto v : myVert)
     {
-        S.line({(int)(scale * v), 0, (int)(scale * v), (int)(scale * myHorz.back())});
+        S.line({(int)(sc * v), 0, (int)(sc * v), (int)(sc * myHorz.back())});
     }
 
     for (auto &f : myForbidden)
     {
         auto fDim = rectDim(f);
 
-        S.line({(int)(scale * fDim[0]), (int)(scale * fDim[1]),
-                (int)(scale * fDim[2]), (int)(scale * fDim[3])});
-        S.line({(int)(scale * fDim[2]), (int)(scale * fDim[1]),
-                (int)(scale * fDim[0]), (int)(scale * fDim[3])});
+        S.line({(int)(sc * fDim[0]), (int)(sc * fDim[1]),
+                (int)(sc * fDim[2]), (int)(sc * fDim[3])});
+        S.line({(int)(sc * fDim[2]), (int)(sc * fDim[1]),
+                (int)(sc * fDim[0]), (int)(sc * fDim[3])});
     }
 
     auto startCellDim = rectDim(myStartCell);
     S.text("S",
-           {(int)(scale * startCellDim[0] + 5),
-            (int)(scale * startCellDim[1] + 5)});
+           {(int)(sc * startCellDim[0] + 5),
+            (int)(sc * startCellDim[1] + 5)});
+}
 
+void cGrid::drawCombo(
+    wex::label &lbCombo,
+    wex::shapes &S)
+{
+    lbCombo.text(std::to_string(myCurComb + 1) + " of " + std::to_string(myListComb.size()));
     S.color(0xFF0000);
-    int icomb = 0;
-    for (auto &c : myListComb[icomb])
+    for (auto &c : myListComb[myCurComb])
     {
-        drawRectangle(S, scale, rectDim(c));
+        drawRectangle(S, scale(), rectDim(c));
     }
 }
+
+std::string textCombo();
 
 void cGrid::EnumerateCombinations()
 {
@@ -208,6 +248,7 @@ void cGrid::EnumerateCombinations()
             myListComb.push_back(comb);
         }
     }
+    myCurComb = 0;
     std::cout << myListComb.size() << " valid cell combinations found\n";
     for (auto &comb : myListComb)
     {
